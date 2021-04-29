8f2f4589d04fc43a7fbb95e0ec33a1998e45c23b
Butterfill <Peter.Butterfill@parexel.com>
2021-04-29 15:22:39 +0100

    fixing CI dependencies

--- smart_on_fhir_client_py_demo/_nbdev.py
+++ smart_on_fhir_client_py_demo/_nbdev.py
@@ -17,8 +17,8 @@ modules = ["core.py",
            "flask_hello_world.py",
            "flask_client_py_readme.py"]
 
-doc_url = "https://pete88b.github.io/smart_on_fhir_client_py_demo/"
+doc_url = "https://pete88b.github.io/smart-on-fhir-client-py-demo/"
 
-git_url = "https://github.com/pete88b/smart_on_fhir_client_py_demo/tree/main/"
+git_url = "https://github.com/pete88b/smart-on-fhir-client-py-demo/tree/main/"
 
 def custom_doc_links(name): return None
